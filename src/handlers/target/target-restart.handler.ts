c2444b746826c148eae51d0b4902322a1d94e3b8
johncmerfeld <john.merfeld@gmail.com>
2022-08-25 09:44:28 -0400

    pulling latest

--- src/handlers/target/target-restart.handler.ts
+++ src/handlers/target/target-restart.handler.ts
@@ -5,6 +5,8 @@ import { restartArgs } from './target-restart.command-builder';
 import yargs from 'yargs';
 import { cleanExit } from '../clean-exit.handler';
 import { BzeroTargetHttpService } from '../../http-services/targets/bzero/bzero.http-services';
+import { EventsHttpService } from '../../http-services/events/events.http-server';
+import { TargetType } from '../../../webshell-common-ts/http/v2/target/types/target.types';
 
 export async function targetRestartHandler(
     argv: yargs.Arguments<restartArgs>,
@@ -16,6 +18,7 @@ export async function targetRestartHandler(
     }
 
     const parsedTarget = parseTargetString(argv.targetString);
+
     const bzeroTargetService = new BzeroTargetHttpService(configService, logger);
     await bzeroTargetService.RestartBzeroTarget({
         targetName: parsedTarget.name,
@@ -23,6 +26,13 @@ export async function targetRestartHandler(
         envId: parsedTarget.envId,
         envName: parsedTarget.envName,
     });
+
+    /*
+    const eventService = new EventsHttpService(configService, logger);
+    // TODO: not that this even lives here but obviously need to fix this
+    const x = await eventService.GetAgentStatusChangeEvents(parsedTarget.id, TargetType.Bzero);
+    console.log(x);
+    */
 
     await cleanExit(0, logger);
 }
