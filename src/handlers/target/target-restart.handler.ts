69d2c800a7cd611ed779a80b97beb26e0782698d
johncmerfeld <john.merfeld@gmail.com>
2022-08-29 11:10:19 -0400

    actually add suite

--- src/handlers/target/target-restart.handler.ts
+++ src/handlers/target/target-restart.handler.ts
@@ -5,6 +5,8 @@ import { restartArgs } from './target-restart.command-builder';
 import yargs from 'yargs';
 import { cleanExit } from '../clean-exit.handler';
 import { BzeroTargetHttpService } from '../../http-services/targets/bzero/bzero.http-services';
+import { waitForRestart } from '../..//system-tests/tests/utils/target-utils';
+import { EventsHttpService } from '../../http-services/events/events.http-server';
 
 export async function targetRestartHandler(
     argv: yargs.Arguments<restartArgs>,
@@ -19,6 +21,7 @@ export async function targetRestartHandler(
 
     const bzeroTargetService = new BzeroTargetHttpService(configService, logger);
 
+    const now = new Date(new Date().toUTCString());
     try {
         await bzeroTargetService.RestartBzeroTarget({
             targetName: parsedTarget.name,
@@ -29,6 +32,16 @@ export async function targetRestartHandler(
     } catch (error) {
         logger.error(`error: ${error}`);
     }
+    parsedTarget.name = "john-bzero-agent";
+
+    /* FIXME: remove
+    // first, check that the agent restarted
+    await waitForRestart(configService, logger, parsedTarget);
+
+    const eventService = new EventsHttpService(configService, logger);
+    const newChanges = await eventService.GetAgentStatusChangeEvents(parsedTarget.id, now);
+    console.log(newChanges);
+    */
 
     logger.info(`Agent restart initiated. To monitor your target's status, use: zli lt -d${parsedTarget.name ? ` -n ${parsedTarget.name}` : ` -i`} `)
 
--- src/system-tests/tests/suites/target.ts
+++ src/system-tests/tests/suites/target.ts
@@ -1,30 +1,15 @@
-import path from 'path';
-import fs from 'fs';
-import * as CleanExitHandler from '../../../handlers/clean-exit.handler';
-import { promisify } from 'util';
-import { exec } from 'child_process';
-import { PolicyQueryHttpService } from '../../../http-services/policy-query/policy-query.http-services';
-import { allTargets, configService, logger, systemTestEnvId, loggerConfigService, systemTestPolicyTemplate, systemTestUniqueId } from '../system-test';
+import { configService, logger, systemTestUniqueId } from '../system-test';
 import { callZli } from '../utils/zli-utils';
-import { removeIfExists } from '../../../utils/utils';
+import { parseTargetString } from '../../../utils/utils';
 import { TestUtils } from '../utils/test-utils';
-import { bzeroTargetCustomUser } from '../system-test-setup';
-import { SubjectType } from '../../../../webshell-common-ts/http/v2/common.types/subject.types';
-import { Environment } from '../../../../webshell-common-ts/http/v2/policy/types/environment.types';
 import { TestTarget } from '../system-test.types';
-import { cleanupTargetConnectPolicies } from '../system-test-cleanup';
-import { PolicyHttpService } from '../../../http-services/policy/policy.http-services';
-import { Subject } from '../../../../webshell-common-ts/http/v2/policy/types/subject.types';
-import { VerbType } from '../../../../webshell-common-ts/http/v2/policy/types/verb-type.types';
 import { ConnectTestUtils } from '../utils/connect-utils';
-import { ssmUser, getTargetInfo, expectIncludeStmtInConfig, expectTargetsInBzConfig } from '../utils/ssh-utils';
+import { getTargetInfo } from '../utils/ssh-utils';
 import { bzeroTestTargetsToRun } from '../targets-to-run';
 import { EventsHttpService } from '../../../http-services/events/events.http-server';
-import { TargetType } from '../../../../webshell-common-ts/http/v2/target/types/target.types';
-import { listTargets } from '../../../services/list-targets/list-targets.service';
-import { TargetStatus } from '../../../../webshell-common-ts/http/v2/target/types/targetStatus.types';
+import { waitForRestart } from '../utils/target-utils';
 
-export const sshSuite = () => {
+export const targetSuite = () => {
     describe('target suite', () => {
 
         let testUtils: TestUtils;
@@ -47,37 +32,13 @@ export const sshSuite = () => {
                 const { targetName, targetId } = await getTargetInfo(testTarget);
                 await callZli(['target', 'restart', targetName]);
 
-                let goneOffline = false;
-                let backOnline = false;
-
                 const now = new Date(new Date().toUTCString());
 
                 // first, check that the agent restarted
-                // TODO: factor this out
-                while (!goneOffline) {
-                    const targets = await listTargets(configService, logger, [TargetType.Bzero]);
-                    const myTarget = targets.filter(target => target.name === targetName);
-                    if (myTarget.length !== 1) {
-                        throw new Error(`Expected 1 target but got ${myTarget.length}`);
-                    } else {
-                        goneOffline = myTarget[0].status === TargetStatus.Offline;
-                    }
-                }
-
-                while (!backOnline) {
-                    const targets = await listTargets(configService, logger, [TargetType.Bzero]);
-                    const myTarget = targets.filter(target => target.name === targetName);
-                    if (myTarget.length !== 1) {
-                        throw new Error(`Expected 1 target but got ${myTarget.length}`);
-                    } else {
-                        backOnline = myTarget[0].status === TargetStatus.Online;
-                    }
-                }
+                await waitForRestart(configService, logger, parseTargetString(targetName));
 
                 const eventService = new EventsHttpService(configService, logger);
-                const agentStatusChanges = await eventService.GetAgentStatusChangeEvents(targetId);
-                const newChanges = agentStatusChanges.filter(e => new Date(e.timeStamp).getTime() > now.getTime())
-                    .sort((a, b) => new Date(a.timeStamp).getTime() - new Date(b.timeStamp).getTime());
+                const newChanges = await eventService.GetAgentStatusChangeEvents(targetId, now);
                 expect(newChanges.length).toBe(3);
                 expect(newChanges[0].statusChange).toBe("OnlineToOffline");
                 expect(newChanges[1].statusChange).toBe("OfflineToRestarting");
