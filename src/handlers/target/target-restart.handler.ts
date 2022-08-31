0742058e2a16265101eba7422ae0a55fc7cbe7e5
johncmerfeld <john.merfeld@gmail.com>
2022-08-31 11:55:56 -0400

    remove unused code

--- src/handlers/target/target-restart.handler.ts
+++ src/handlers/target/target-restart.handler.ts
@@ -5,10 +5,6 @@ import { restartArgs } from './target-restart.command-builder';
 import yargs from 'yargs';
 import { cleanExit } from '../clean-exit.handler';
 import { BzeroTargetHttpService } from '../../http-services/targets/bzero/bzero.http-services';
-import { listTargets } from '../../services/list-targets/list-targets.service';
-import { TargetType } from '../../../webshell-common-ts/http/v2/target/types/target.types';
-import { TargetStatus } from '../../../webshell-common-ts/http/v2/target/types/targetStatus.types';
-import { ParsedTargetString } from '../../services/common.types';
 
 export async function targetRestartHandler(
     argv: yargs.Arguments<restartArgs>,
@@ -38,31 +34,4 @@ export async function targetRestartHandler(
     logger.info(`Agent restart initiated. To monitor your target's status, use: zli lt -d${parsedTarget.name ? ` -n ${parsedTarget.name}` : ` -i`} `);
 
     await cleanExit(0, logger);
-}
-
-export async function waitForRestart(configService: ConfigService, logger: Logger, targetString: ParsedTargetString) {
-    let goneOffline = false;
-    let backOnline = false;
-    // TODO: support other target notations
-    while (!goneOffline) {
-        const targets = await listTargets(configService, logger, [TargetType.Bzero]);
-        const myTarget = targets.filter(target => target.name === targetString.name);
-        if (myTarget.length !== 1) {
-            throw new Error(`Expected 1 target but got ${myTarget.length}`);
-        } else {
-            goneOffline = myTarget[0].status === TargetStatus.Offline;
-        }
-    }
-
-    console.log('Gone offline!');
-
-    while (!backOnline) {
-        const targets = await listTargets(configService, logger, [TargetType.Bzero]);
-        const myTarget = targets.filter(target => target.name === targetString.name);
-        if (myTarget.length !== 1) {
-            throw new Error(`Expected 1 target but got ${myTarget.length}`);
-        } else {
-            backOnline = myTarget[0].status === TargetStatus.Online;
-        }
-    }
 }
