8dfc4d02b0a11820a9c2d06305c8aeea4209aaf5
johncmerfeld <john.merfeld@gmail.com>
2022-09-02 12:06:37 -0400

    bring down a timeout

--- src/system-tests/tests/suites/agent-recovery.ts
+++ src/system-tests/tests/suites/agent-recovery.ts
@@ -248,7 +248,7 @@ export const agentRecoverySuite = (testRunnerKubeConfigFile: string, testRunnerU
             // find a online->offline event for this agent
             await testUtils.EnsureAgentStatusEvent(targetId, {
                 statusChange: 'OnlineToOffline'
-            }, testStartTime, undefined, 3 * 60 * 1000);
+            }, testStartTime, undefined, 1 * 60 * 1000);
 
             logger.info('Found online to offline event');
 
