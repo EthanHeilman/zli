9e68631680f07e7352c942a5b628faa519709b0c
Sebby Lipman <sebby@commonwealthcrypto.com>
2022-09-08 12:50:26 -0400

    Update al2 snapshot image again with postgres config fix (#526)

--- src/system-tests/digital-ocean/digital-ocean-ssm-target.service.types.ts
+++ src/system-tests/digital-ocean/digital-ocean-ssm-target.service.types.ts
@@ -24,7 +24,7 @@ export const DigitalOceanDistroImage = {
     // up-to-date and to also rotate the aws credential used by the amazon
     // cloudwatch agent on the snapshot to push agent logs to cloudwatch
     // https://github.com/bastionzero/cwc-infra/wiki/System-Test-Digital-Ocean-Snapshots
-    BzeroVTAL2TestImage: 116411145,
+    BzeroVTAL2TestImage: 116429980,
     BzeroVTUbuntuTestImage: 116083687
 } as const;
 export type DigitalOceanDistroImage = typeof DigitalOceanDistroImage[keyof typeof DigitalOceanDistroImage];
