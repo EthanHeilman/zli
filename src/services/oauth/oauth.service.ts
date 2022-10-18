b5b2a91fd8a6cbea47fa82311baf1b79f4df1f27
tflpd <aflpd@bu.edu>
2022-10-18 10:23:42 -0400

    Removed call to user/me from inside register call so when mfa is enabled we firstly mfa and then call user/me

--- src/services/oauth/oauth.service.ts
+++ src/services/oauth/oauth.service.ts
@@ -347,6 +347,10 @@ export class OAuthService implements IDisposable {
 
                 const userHttpService = new UserHttpService(this.configService, this.logger);
                 await userHttpService.Register();
+                // Update me section of the config in case this is a new login or any
+                // user information has changed since last login
+                const me = await userHttpService.Me();
+                this.configService.setMe(me);
             }
         } else {
             throw new UserNotLoggedInError();
